a466f03f5980b21d412cf7b4e9131af1db0d3c91
JOSEPH ABRAHAM SOTO GUTIERREZ <jasotog@correo.url.edu.gt>
2019-03-27 16:00:00 -0600

    Proyecto Plaza - Movido a carpeta raíz

---
+++ Plaza Pública/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#carousel').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',    
+        autoplay: true,
+        autoplaySpeed: 1800
+    });
+  });
