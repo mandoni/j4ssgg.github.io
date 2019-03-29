66f2b60e5d3adff8d816612e72b570a2a873f581
JOSEPH ABRAHAM SOTO GUTIERREZ <jasotog@correo.url.edu.gt>
2019-03-28 18:56:02 -0600

    Proyecto Plaza - Nota 1 elementos para las graficas anadidos

--- Plaza Pública/js/nota1.js
+++ Plaza Pública/js/nota1.js
@@ -28,6 +28,8 @@ $(document).ready(function () {
                 color: text
             }
         },
+        autosize: 'false',
+        height: 600,
         xaxis: {
             font: {
                 family: 'TisaPro',
@@ -78,13 +80,15 @@ $(document).ready(function () {
 
     var grafico1_layout = {
         title: {
-            text: 'Contratos',
+            text: 'Contratos 2004-2008',
             font: {
                 family: 'Pluto Bold',
                 size: 18,
                 color: text
             }
         },
+        autosize: 'false',
+        height: 600,
         xaxis: {
             font: {
                 family: 'TisaPro',
@@ -104,10 +108,10 @@ $(document).ready(function () {
 
 
     grafico2_x = [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018];
-    grafico2_y1 = [1124200000,1160000000,1288800000,1301800000,1634800000,1886600000,1972800000,2275200000,2347622265,2720828434,3092105375,2900935861,3251931133,0,0];
-    grafico2_y2 = [0,56016131.67,124880671.6,54692233.31,75274309.6,232720155.1,390351444.3,327975647.2,164734312.2,499897135.3,677375582.8,146424042.6,64571172.51,431323273,83495469.21];
-    grafico2_y3 = [0,0,0,0,0,15657868.07,55294828.5,7232355.3,60323597.49,88764641.24,170571,8000,2801977.93,6315244.07,81543.47];
-    
+    grafico2_y1 = [1124200000, 1160000000, 1288800000, 1301800000, 1634800000, 1886600000, 1972800000, 2275200000, 2347622265, 2720828434, 3092105375, 2900935861, 3251931133, 0, 0];
+    grafico2_y2 = [0, 56016131.67, 124880671.6, 54692233.31, 75274309.6, 232720155.1, 390351444.3, 327975647.2, 164734312.2, 499897135.3, 677375582.8, 146424042.6, 64571172.51, 431323273, 83495469.21];
+    grafico2_y3 = [0, 0, 0, 0, 0, 15657868.07, 55294828.5, 7232355.3, 60323597.49, 88764641.24, 170571, 8000, 2801977.93, 6315244.07, 81543.47];
+
     var grafico2 = [
         {
             x: grafico2_x,
@@ -151,13 +155,15 @@ $(document).ready(function () {
 
     var grafico2_layout = {
         title: {
-            text: 'Contratos',
+            text: 'Seguridad pública versus seguridad privada',
             font: {
                 family: 'Pluto Bold',
                 size: 18,
                 color: text
             }
         },
+        autosize: 'false',
+        height: 600,
         xaxis: {
             font: {
                 family: 'TisaPro',
@@ -173,7 +179,7 @@ $(document).ready(function () {
             }
         }
     };
-    
+
 
     grafico3_x = [
         'CONDOR S.A.',
@@ -193,19 +199,19 @@ $(document).ready(function () {
         'TOTAL GENERAL'
     ];
 
-    grafico3_y2004 = [0,0,0,0,0,0,0,0,0,0,0,0,0,158200,158200];
-    grafico3_y2005 = [0,0,0,0,0,0,351456,0,13055205,0,433800,0,0,0,13840461];
-    grafico3_y2006 = [0,3379200,0,0,0,2980800,642432,0,595000,0,0,0,0,0,7597432];
-    grafico3_y2007 = [0,3801600,0,0,0,0,,0,7095600,0,0,0,0,0,10897200];
-    grafico3_y2008 = [0,0,0,0,0,0,0,0,225000,0,0,0,0,0,225000];
-    grafico3_y2009 = [0,0,747500,0,0,0,0,0,19101090,0,0,0,816750,0,20665340    ];
-    grafico3_y2010 = [0,0,0,0,715000,0,0,0,0,0,0,0,2543400,0,3258400];
-    grafico3_y2011 = [0,0,0,0,0,0,0,0,0,480000,0,0,3445200,0,3925200];
-    grafico3_y2012 = [0,0,0,0,0,0,0,0,0,0,171360,78500,0,0,249860];
-    grafico3_y2013 = [0,0,0,0,0,0,0,0,0,0,276000,0,74000,0,350000];
-    grafico3_y2014 = [267000,0,0,720000,0,0,0,0,0,0,276000,0,0,0,1263000];
-    grafico3_y2017 = [0,0,0,0,0,0,0,72000,0,0,0,0,0,0,72000];
-    grafico3_ytotal = [267000,7180800,747500,720000,715000,2980800,993888,72000,40071895,480000,1157160,78500,6879350,158200,62502093];
+    grafico3_y2004 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 158200, 158200];
+    grafico3_y2005 = [0, 0, 0, 0, 0, 0, 351456, 0, 13055205, 0, 433800, 0, 0, 0, 13840461];
+    grafico3_y2006 = [0, 3379200, 0, 0, 0, 2980800, 642432, 0, 595000, 0, 0, 0, 0, 0, 7597432];
+    grafico3_y2007 = [0, 3801600, 0, 0, 0, 0, , 0, 7095600, 0, 0, 0, 0, 0, 10897200];
+    grafico3_y2008 = [0, 0, 0, 0, 0, 0, 0, 0, 225000, 0, 0, 0, 0, 0, 225000];
+    grafico3_y2009 = [0, 0, 747500, 0, 0, 0, 0, 0, 19101090, 0, 0, 0, 816750, 0, 20665340];
+    grafico3_y2010 = [0, 0, 0, 0, 715000, 0, 0, 0, 0, 0, 0, 0, 2543400, 0, 3258400];
+    grafico3_y2011 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 480000, 0, 0, 3445200, 0, 3925200];
+    grafico3_y2012 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 171360, 78500, 0, 0, 249860];
+    grafico3_y2013 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276000, 0, 74000, 0, 350000];
+    grafico3_y2014 = [267000, 0, 0, 720000, 0, 0, 0, 0, 0, 0, 276000, 0, 0, 0, 1263000];
+    grafico3_y2017 = [0, 0, 0, 0, 0, 0, 0, 72000, 0, 0, 0, 0, 0, 0, 72000];
+    grafico3_ytotal = [267000, 7180800, 747500, 720000, 715000, 2980800, 993888, 72000, 40071895, 480000, 1157160, 78500, 6879350, 158200, 62502093];
 
     var grafico3_2004 = [
         {
@@ -396,13 +402,15 @@ $(document).ready(function () {
     var grafico3_layout = {
 
         margin: {
-            t: 75,
+            t: 100,
             b: 270,
             r: 125
-          },
+        },
+        autosize: 'false',
+        height: 600,
 
         title: {
-            text: 'Contratos',
+            text: 'Empresas con acuerdo gubernamental',
             font: {
                 family: 'Pluto Bold',
                 size: 18,
@@ -410,7 +418,7 @@ $(document).ready(function () {
             }
         },
         xaxis: {
-            
+
             ticks: 'outside',
             tickangle: 45,
             font: {
@@ -449,15 +457,20 @@ $(document).ready(function () {
 
     var grafico4_layout = {
         title: {
-            text: 'Centroamérica: Muertes causadas con armas de fuego 2014',
+            text: 'Los contratistas de las empresas con acuerdo',
             font: {
                 family: 'Pluto Bold',
                 size: 18,
                 color: text
             }
         },
+        margin: {
+            t: 0,
+            b: 210
+        },
+        autosize: 'false',
+        height: 600,
         xaxis: {
-            showticklabels: false,
             font: {
                 family: 'TisaPro',
                 size: 14,
@@ -477,7 +490,7 @@ $(document).ready(function () {
     Plotly.newPlot('imagen1', imagen1, imagen1_layout, { responsive: true, displayModeBar: false });
 
     Plotly.newPlot('grafico1', grafico1, grafico1_layout, { responsive: true, displayModeBar: false });
-    
+
     Plotly.newPlot('grafico2', grafico2, grafico2_layout, { responsive: true, displayModeBar: false });
 
     // Graficos 3 en landing
@@ -495,7 +508,7 @@ $(document).ready(function () {
     Plotly.newPlot('grafico3_2014', grafico3_2014, grafico3_layout, { responsive: true, displayModeBar: false });
     Plotly.newPlot('grafico3_2017', grafico3_2017, grafico3_layout, { responsive: true, displayModeBar: false });
     Plotly.newPlot('grafico3_total', grafico3_total, grafico3_layout, { responsive: true, displayModeBar: false });
-   
+
 
     Plotly.newPlot('grafico4', grafico4, grafico4_layout, { responsive: true, displayModeBar: false });
 
---
+++ Plaza Pública/js/nota2.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    let celeste = '#4CC4CE';
+    let cian = '#2CBEC9';
+    let red = '#EE6A5A';
+    let gray = '#939393';
+    let text = '#333333';
+
+
+    
+
+
+
+});
