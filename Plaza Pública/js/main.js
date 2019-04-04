042d2fcb82ba8f547df561c49a3a035667034cb8
JOSEPH ABRAHAM SOTO GUTIERREZ <jasotog@correo.url.edu.gt>
2019-04-03 20:33:18 -0600

    Proyecto Plaza - Contador añadido

--- Plaza Pública/js/main.js
+++ Plaza Pública/js/main.js
@@ -1,26 +1,45 @@
-$(document).ready(function(){
-    $('#carousel').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 1000,
-        fade: true,
-        cssEase: 'linear',    
-        autoplay: true,
-        autoplaySpeed: 2500
-    });
-  });
 
 
-  $(document).ready(function(){
-    $('#carousel-2').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 1000,
-        fade: true,
-        cssEase: 'linear',    
-        autoplay: true,
-        autoplaySpeed: 2500
-    });
+$(document).ready(function () {
+  $('#carousel').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2500
   });
+});
+
+
+$(document).ready(function () {
+  $('#carousel-2').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2500
+  });
+});
+
+
+
+// Contador
+
+
+$(document).ready(function () {
+  actual = (new Date(Date.now())).getTime();
+  pub = (new Date("March 05, 2019 00:00:00")).getTime();  
+
+  var times = Math.round((actual - pub)/1000, 0);
+  var balas = document.getElementById("balas");
+  window.setInterval(function () {
+    balas.innerHTML = times;
+    times++;
+  }, 1000);
+});
