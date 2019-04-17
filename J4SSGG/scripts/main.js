a6ec6b4456b35a6a87751ca3ffbb8381d2191b8c
JOSEPH ABRAHAM SOTO GUTIERREZ <jasotog@correo.url.edu.gt>
2019-04-16 18:53:13 -0600

    J4SSGG - Agregado sitio principal

---
+++ J4SSGG/scripts/main.js
@@ -0,0 +1,36 @@
+// Only load main functions when document is ready :D
+
+$(document).ready(function(){
+
+    // main Time object
+
+    
+
+    /**
+     * 
+     * @param {number} element jQuery span object
+     * 
+     * 
+     */
+    function setTimeInto(element){
+        element.html(getTime());
+    }
+
+
+
+    /**
+     * @param {Object} clock a new instance of Date();
+     * @returns {string} with time in HH:MM:SS format
+     */
+    function getTime(){
+        
+        return (new Date()).toLocaleTimeString();
+    }
+
+
+
+    // Update time every second 
+    window.setInterval(setTimeInto, 1000, $("#time"));
+    
+
+});
