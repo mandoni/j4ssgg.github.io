a229570a77968a22576508948ab4e3e62a8c47cb
JOSEPH ABRAHAM SOTO GUTIERREZ <jasotog@correo.url.edu.gt>
2019-04-18 00:28:53 -0600

    J4SSGG - J4: euclidean distances and K-nearest-Neighbors added to simulate lines behavior

--- J4SSGG/scripts/J4.js
+++ J4SSGG/scripts/J4.js
@@ -1,16 +1,21 @@
 // Simple NET project using Canvas and pure JavaScript
 // author: @J4SSGG - Abraham Gutierrez
 
-
-let dotColor = "#fff";
-let lineColor = "#3f206f";
-let amount = 50;
-
 let canvas;
 let ctx;
+
+let dotColor = "#E8444D";
+let lineColor = "#E8444D";
+
+
 let dots = [];
+
+let amount = 90;
 let width = 0;
 let height = 0;
+let xV = 5;
+let yV = 5;
+let size = 4;
 
 class J4 {
 
@@ -25,11 +30,10 @@ class J4 {
 
     constructor(canvasID, configuration) {
         canvas = document.getElementById(canvasID);
-        canvas.width = canvas.clientWidth; // fix to blur
-        canvas.height = canvas.clientHeight; // fix to blur
+        canvas.width = document.body.clientWidth; // fix to blur
+        canvas.height = window.innerHeight; // fix to blur
         ctx = canvas.getContext("2d");
 
-        dots = [];
         width = canvas.width;
         height = canvas.height;
 
@@ -53,23 +57,93 @@ class J4 {
         //fill again
         for (var i = 0; i < amount; i++) {
             dots.push({
+                id: i,
                 x: Math.random() * width,
                 y: Math.random() * height,
-                vx: Math.random() * 10 - 5,
-                vy: Math.random() * 10 - 5,
+                vx: Math.random() * xV,
+                vy: Math.random() * yV,
+                size: this.random(1, size),
+                neighbors: []
             })
         }
     }
 
 
-    draw() {
-        ctx.clearRect(0, 0, width, height);
+    /**
+     * 
+     * @param {Object} A one dot object with x,y coordinates
+     * @param {Object} B one dot object with x,y coordinates
+     * @returns {number} 
+     */
+    euclideanDistance(A, B) {
+        return Math.sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
+    }
+
+
+    random(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    compare(a, b) {
+        if (a.distance < b.distance) {
+            return -1;
+
+        }
+
+        if (a.distance == b.distance) {
+            return 0;
+        }
+
+        return 1;
+    }
+
+    /**
+     * 
+     * @param {number} K number of neighbors considered
+     * 
+     */
+    neighbors(K) {
+        for (var i = 0; i < amount; i++) {
+            dots[i].neighbors = [];
+            for (var j = i + 1; j < amount; j++) {
+                dots[i].neighbors.push(
+                    {
+                        id: j,
+                        distance: this.euclideanDistance(dots[i], dots[j])
+                    }
+                );
+            }
+            dots[i].neighbors = dots[i].neighbors.sort(this.compare).slice(0, K);
+        }
+    }
+
+    drawDots() {
         dots.forEach(function (dot) {
             ctx.beginPath();
-            ctx.arc(dot.x, dot.y, 1, 0, Math.PI * 2, false);
+            ctx.arc(dot.x, dot.y, dot.size, 0, Math.PI * 2, false);
             ctx.fillStyle = dotColor;
             ctx.fill();
         });
     }
 
+    drawLines() {
+        this.neighbors(4);
+        dots.forEach(function (dot) {
+
+            ctx.beginPath();
+            ctx.strokeStyle = lineColor;
+
+            dot.neighbors.forEach(function (neighbor) {
+                ctx.lineWidth = 0.25 - 1 / neighbor.distance;
+                ctx.moveTo(dot.x, dot.y);
+                ctx.lineTo(dots[neighbor.id].x, dots[neighbor.id].y);
+                ctx.stroke();
+            });
+        });
+    }
+
+    draw() {
+        return Promise.all([ctx.clearRect(0, 0, width, height),
+        this.drawLines(), this.drawDots()]);
+    }
 }
