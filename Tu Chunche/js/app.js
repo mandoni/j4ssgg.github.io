517554bd63c85208e1bf5d913d808d8328aaf357
Andoni Zamora <tonytaze096@gmail.com>
2019-09-19 08:46:07 -0600

    Camera shot: button & JS

--- Tu Chunche/js/app.js
+++ Tu Chunche/js/app.js
@@ -28,6 +28,22 @@ $('#changeCamera').click(function(){
     }
 }); 
 
+///colocar la imagen del stream en div:preview 
+$('#shoot').click(function(){
+    $video = document.querySelector('video');
+    $video.pause();
+
+    //Obtener contexto del canvas y dibujar sobre él
+    $canvas = document.querySelector('canvas')
+    let contexto = $canvas.getContext("2d");
+    $canvas.width = $video.videoWidth;
+    $canvas.height = $video.videoHeight;
+    contexto.drawImage($video, 0, 0, $canvas.width, $canvas.height);
+
+    $camera = document.querySelector('#camera');
+    $camera.hide();
+    
+});
 
 /// actualizar nombre de input file
 // generar priview
@@ -36,6 +52,8 @@ jQuery('input[type=file]').change(function(){
     $('#imageName').html(jQuery(this).val().split('\\').pop());
     filePreview($('input[type=file]')[0]);
 });
+
+
 
 
 function filePreview(input) {
